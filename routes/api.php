7255c8d3d5e9277eb3973da4f39a5990dff59086
csemiraz <mirazul2013@gmail.com>
2022-06-13 13:02:16 +0600

    migrating revews and products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ProductController;
 
 /*
@@ -21,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('products', ProductController::class);
 
-/* Route::group(['prefix'=>'products', function() {
+Route::group(['prefix'=>'products'], function() {
     Route::apiResource('/{product}/reviews', ReviewController::class);
-}]); */
+});
+
